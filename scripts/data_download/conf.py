e72d2062bc496b1dba4a525ca6edea13fca0bb62
Olivier Bonte <olivier.bonte@hotmail.com>
2026-05-22 17:33:44 +0200

    Add waterinfo download WIP

--- scripts/data_download/conf.py
+++ scripts/data_download/conf.py
@@ -16,3 +16,14 @@ CRS = "http://www.opengis.net/def/crs/EPSG/0/31370"
 XMIN, XMAX, YMIN, YMAX = 98_000, 116_000, 160_000, 180_000  # extent in m (EPSG:31370)
 NO_DATA_VALUE_DTM = -9999.0
 WCS_ENDPOINT_DTM = "https://geo.api.vlaanderen.be/dhmv/wcs"
+
+# Meteorology station info
+STATION_ID_MAARKE_KERKEM = "P06_014"
+
+
+# Discharge station info
+STATION_ID_NEDERZWALM = "L06_342"
+PRECIP_CATCHMENT_LONGNAME = "Precipitation of catchment"
+PRECIP_CATCHMENT_TS_NAME = "Rro.DagTot"
+START_YEAR = 2009
+END_YEAR = 2025
---
+++ scripts/data_download/waterinfo_download.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from conf import END_YEAR, START_YEAR, STATION_ID_MAARKE_KERKEM, STATION_ID_NEDERZWALM
+from pywaterinfo import Waterinfo
+
+vmm = Waterinfo("vmm", cache=True)
+station_info_mk = vmm.get_timeseries_list(STATION_ID_MAARKE_KERKEM)
+station_info_nz = vmm.get_timeseries_list(STATION_ID_NEDERZWALM)
+
+#
+station_info_nz_precip = station_info_nz.query(
+    "stationparameter_longname == 'Precipitation of catchment' and ts_name == 'Rro.DagTot'"
+)
+# date_cols = ["from", "to"]
+# for col in date_cols:
+#     station_info_nz_precip[col] = pd.to_datetime(station_info_nz_precip[col])
+
+# start_year_nz_precip = station_info_nz_precip["from"].dt.year.values[0] + 1
+# end_year_nz_precip = station_info_nz_precip["to"].dt.year.values[0] - 1
+ts_id_nz_precip = station_info_nz_precip["ts_id"].values[0]
+_start_date = f"{START_YEAR}-01-01"
+_end_date = f"{END_YEAR}-01-31"
+df_nz_precip = (
+    vmm.get_timeseries_values(
+        ts_id=ts_id_nz_precip,
+        start=_start_date,
+        end=_end_date,
+    )
+    .reset_index()
+    .assign(
+        Timestamp=lambda df: pd.to_datetime(pd.to_datetime(df["Timestamp"]).dt.date)
+    )
+    .set_index("Timestamp")
+)

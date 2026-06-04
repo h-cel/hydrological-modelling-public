f8084e2312ba643756534d7d763b5a84ceab322e
Olivier Bonte <olivier.bonte@hotmail.com>
2026-06-04 11:20:19 +0200

    Add satellite soil moisture filtering

--- scripts/data_process/satellite_soil_moisture_process.py
+++ scripts/data_process/satellite_soil_moisture_process.py
@@ -6,11 +6,21 @@ from conf import (
     CATCHMENT_INFO_PROCESSED_DIR,
     CATCHMENT_NAME,
     FILENAME_AFSTROOMGEBIED,
+    FILENAME_SATELLITE_SOIL_MOISTURE,
+    MAX_PCT_RISE_PER_DAY,
     MIN_COVERAGE_RATIO,
+    MIN_PCT_DROP_PER_DAY,
+    SATELLITE_SOIL_MOISTURE_PROCESSED_DIR,
     SATELLITE_SOIL_MOISTURE_RAW_DIR,
+    VARIABLE_NAME_SATELLITE_SOIL_MOISTURE,
+    logger,
 )
 
-if __name__ == "__main__":
+SATELLITE_SOIL_MOISTURE_PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
+
+
+# %% Main function
+def main():
     # %% Read in
     ds = xr.open_mfdataset(
         SATELLITE_SOIL_MOISTURE_RAW_DIR.glob("*/*.nc"),
@@ -24,10 +34,13 @@ if __name__ == "__main__":
     ]
 
     # %% Create reference DataArray with full spatial coverage
-    da_ref = ds["ssm"].isel(time=0).copy()
+    da_ref = ds[VARIABLE_NAME_SATELLITE_SOIL_MOISTURE].isel(time=0).copy()
     da_ref.values = np.random.rand(*da_ref.shape)  # Fill with random values
 
     # %% Clip surface soil moisture data to catchment area
+    logger.info(
+        f"Clipping surface soil moisture data to catchment area: {CATCHMENT_NAME}"
+    )
     ds_clipped = ds.rio.clip(
         gdf_catchment.geometry.values, gdf_catchment.crs, all_touched=True
     )
@@ -36,8 +49,41 @@ if __name__ == "__main__":
     )
 
     # %% Only keep pixels with coverage more than the minimum coverage ratio
+    logger.info(
+        f"Removing time steps with less than {MIN_COVERAGE_RATIO:.0%} spatial coverage."
+    )
     nr_pixels_full_coverage = da_ref_clipped.count().item()
     nr_pixels_min = int(np.ceil(MIN_COVERAGE_RATIO * nr_pixels_full_coverage))
-    nr_pixels_clipped = ds_clipped["ssm"].count(dim=["lat", "lon"])
+    nr_pixels_clipped = ds_clipped[VARIABLE_NAME_SATELLITE_SOIL_MOISTURE].count(
+        dim=["lat", "lon"]
+    )
     bool_time_steps_to_keep = nr_pixels_clipped >= nr_pixels_min
     ds_clipped_filtered = ds_clipped.sel(time=bool_time_steps_to_keep)
+
+    # %% Average surface soil moisture over catchment area
+    logger.info("Averaging surface soil moisture over catchment area.")
+    ds_spatial_avg = ds_clipped_filtered.mean(dim=["lat", "lon"], skipna=True).compute()
+
+    # %% Filter out too large fluctuations
+    logger.info("Filtering out unrealistic fluctuations in surface soil moisture")
+    time_diff_days = ds_spatial_avg["time"].diff(dim="time").dt.total_seconds() / 86400
+    pct_change_per_day = (
+        ds_spatial_avg[VARIABLE_NAME_SATELLITE_SOIL_MOISTURE].diff(dim="time")
+        / time_diff_days
+    ).reindex(time=ds_spatial_avg.time, fill_value=0)
+    flag_outliers = (pct_change_per_day < MIN_PCT_DROP_PER_DAY) | (
+        pct_change_per_day > MAX_PCT_RISE_PER_DAY
+    ).compute()
+    ds_spatial_avg_filtered = ds_spatial_avg.where(~flag_outliers, drop=True)
+
+    # %% Save to csv
+    path_save = SATELLITE_SOIL_MOISTURE_PROCESSED_DIR / FILENAME_SATELLITE_SOIL_MOISTURE
+    logger.info(f"Saving processed surface soil moisture data to {path_save}")
+    ds_spatial_avg_filtered.to_dataframe().drop(["spatial_ref", "crs"], axis=1).to_csv(
+        path_save
+    )
+
+
+# %% Run main
+if __name__ == "__main__":
+    main()

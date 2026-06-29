c17de1338d7815a40e2b2fd75a4ec8de95bae197
Olivier Bonte <olivier.bonte@hotmail.com>
2026-06-29 15:37:34 +0200

    Change parameter range alpha:
    In the past, alpha could go above 1. Physically this does not make sense = you can create rain and make P_1 negative. Now capped to 1.

--- data/parameter_conversion.py
+++ data/parameter_conversion.py
@@ -1,10 +1,13 @@
+# %% Imports
 import pandas as pd
 
+# %% Read in and definitions
 df_param_old = pd.read_csv("parameters_old.csv")
 seconds_per_day = 24 * 3600
 mm_per_m = 1000
 area_catchment_m2 = 115_000_000  # m^2 (approximate area of the catchment)
 
+# %% Change units
 df_param_new = df_param_old.copy()
 units_old = ["m^3", "m^3/s", "s^-1"]
 units_new = ["mm", "mm/d", "d^-1"]
@@ -23,7 +26,16 @@ for unit_old, unit_new in zip(units_old, units_new):
     )
     df_param_new.loc[df_param_new["Units"] == unit_old, "Units"] = unit_new
 
-# Round the numerical values to 2 significant digits
+# %% Change alpha parameter values
+min_alpha, max_alpha, default_alpha = 0.5, 1.0, 0.8
+df_param_new.loc[df_param_new["Parameter"] == "alpha", numerical_columns] = [
+    min_alpha,
+    max_alpha,
+    default_alpha,
+]
+
+
+# %%  Round the numerical values to 2 significant digits
 df_param_new[numerical_columns] = df_param_new[numerical_columns].apply(
     lambda col: col.apply(lambda x: float(f"{x:.2g}"))
 )
